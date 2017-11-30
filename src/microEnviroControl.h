c9350cc046ec2ca850c7154f47495153e0981189
ghumphr1 <ghumphr1@yahoo.co.uk>
2017-11-30 21:25:49 +0000

    Add files via upload

---
+++ src/microEnviroControl.h
@@ -0,0 +1,29 @@
+/**
+ * microEnviroControl Firmware
+ * Copyright (C) 2017 [https://github.com/ghumphr1/microEnviroControl]
+ *
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+#ifndef microEnviroControl_H
+#define microEnviroControl_H
+
+#include <math.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <inttypes.h>
+
+#endif //microEnviroControl_H
