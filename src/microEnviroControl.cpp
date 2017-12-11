45986108ba2df3df4c9dc483b5d4cb2ae768276a
Geoff Norgrove-Humphries <ghumphr1@GeoffVaio.home>
2017-12-11 17:27:16 +0000

    Signed-off-by: Geoff Norgrove-Humphries <ghumphr1@GeoffVaio.home>

--- src/microEnviroControl.cpp
+++ src/microEnviroControl.cpp
@@ -1,18 +1,23 @@
-#include "Arduino.h"
-#include "microEnviroControl.h"
+#include <Arduino.h>
+#include <LiquidCrystal.h>
+#include <math.h>
 
 //int pinOut = 10; //relay
 int ThermistorPin = 0;
 int Vo;
-float R1 = 10000;
+float R1 = 100000;
 float logR2, R2, T;
 float c1 = 1.009249522e-03, c2 = 2.378405444e-04, c3 = 2.019202697e-07;
 
 
-LiquidCrystal lcd(12, 11, 10, 5, 4, 3, 2);
+LiquidCrystal lcd(12, 11, 5, 4, 3, 2);
 
 void setup() {
-    Serial.begin(9600);
+   Serial.begin(9600);
+    lcd.begin(16, 4);
+    lcd.clear();
+    lcd.print("Current room");
+
 //    pinMode(10, OUTPUT)
 
 }
@@ -23,11 +28,12 @@ void loop() {
   logR2 = log(R2);
   T = (1.0 / (c1 + c2*logR2 + c3*logR2*logR2*logR2));
   T = T - 273.15;
-  T = (T * 9.0)/ 5.0 + 32.0;
+
+  lcd.setCursor(0, 1);
 
   lcd.print("Temp = ");
   lcd.print(T);
-  lcd.print(" F");
+  lcd.print(" C");
  //if (temp >= TempReq){
 //   digitalWrite(pinOut, LOW);
 // }
@@ -35,5 +41,5 @@ void loop() {
 //   digitalWrite(pinOut, HIGH);
  //}
  delay(500);
- lcd.clear();
+ //lcd.clear();
 }
